c35fb202f6ede7b87a6daa1495c17795e95163ef
Diwanshu Shekhar <diwanshu@gmail.com>
2019-02-02 16:57:13 -0700

    buf fix: UnicodeDecodeError

--- pysolrq/solr.py
+++ pysolrq/solr.py
@@ -549,7 +549,7 @@ class SolrControl(SolrClient):
             try:
                 docs = docs + "<field name='{0}'>{1}</field>".format(k, v)
             except UnicodeEncodeError:
-                docs = docs + "<field name='{0}'>{1}</field>".format(k, v.encode('utf-8'))
+                docs = docs + "<field name='{0}'>{1}</field>".format(k, v.encode('utf-8').decode('utf-8'))
 
         docs = "<doc>" + docs + "</doc>"
         return docs
